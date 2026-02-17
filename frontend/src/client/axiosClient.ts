f972191d672a98a56a4fd983cd2f5fc671d0ed1f
Ankit Raj <ankitraj07032002@gmail.com>
2026-02-17 15:23:03 +0530

    removed localhost form the axiosClient

--- frontend/src/client/axiosClient.ts
+++ frontend/src/client/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:8000';
+const BASE_URL = import.meta.env.REACT_APP_API_URL;
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
