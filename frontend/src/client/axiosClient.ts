266afddaae8252e519b5f2d81776034ed303bddf
Ankit Raj <ankitraj07032002@gmail.com>
2026-02-17 17:38:22 +0530

    REACT_APP changed to VITE_APP

--- frontend/src/client/axiosClient.ts
+++ frontend/src/client/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = import.meta.env.REACT_APP_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
